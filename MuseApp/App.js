ac1067aa0a5736e8459044b7e0f244ae87e1221a
tripledots29 <peter.d.stuart29@gmail.com>
2022-03-02 13:51:48 +0000

    status bar skeleton

--- MuseApp/App.js
+++ MuseApp/App.js
@@ -5,26 +5,41 @@ export default function App() {
 
 console.log("App Executed");
 
-  return (
-    <View 
-    style = {{
-      backgroundColor: "dodgerblue",
-      flex: 1,
-    }}
-    >
-      <View style = {{
-        backgroundColor: "white",
-        flex: 1,
-
-      }}/>
-
-      
-      <View style = {{
-        backgroundColor: "dodgerblue",
-        flex: 6
-
-      }}/>
-
+  return (  
+        <View
+        style={{
+          flexDirection: "row",
+          height: 50,
+          justifyContent:"center",
+         
+        }}
+       >
+        <View style={{ backgroundColor: "blue", flex: 1 }}   >
+        <Image
+          style={styles.topImage}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        </View>
+  
+        <View style={{ backgroundColor: "red", flex: 1 }} >
+        <Image
+          style={styles.topImage}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+        </View>
+  
+        <View style={{ backgroundColor: "green", flex: 1 }} >
+        <Image
+          style={styles.topImage}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+       </View>
     </View>
   );
 }
@@ -36,4 +51,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  topImage: {
+    width: 50,
+    height: 50,
+    marginLeft:20
+  },
 });
+
+
+{/*<View 
+    style = {{
+      backgroundColor: "dodgerblue",
+      flex: 1,
+    }}
+    >
+      <View style = {{
+        backgroundColor: "white",
+        flex: 1,
+
+      }}/>
+
+
+      <View style = {{
+        backgroundColor: "dodgerblue",
+        flex: 6
+
+      }}/>
+
+    </View>*/}
